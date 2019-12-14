163a5a2f750400d6f31f821d24b892a982a5402b
peterregan <53829683+peterregan@users.noreply.github.com>
2019-12-13 20:05:00 -0500

    finshed prelim about me page
    added new better styles for about me page.

--- src/components/About.js
+++ src/components/About.js
@@ -1,54 +1,58 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import {withStyles} from "@material-ui/core";
-import '../stylesheets/About.css'
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core";
+import "../stylesheets/About.css";
 
-{/* styles */}
+const aboutparaText = `My name is Peter Regan and here is a little about myself. 
+I am currently a Junior at Hobart and William Smith Colleges 
+studying to obtain a B.S. in computer Science coupled with 
+complementary Mathematics and Physics Minors.
+I am currently seeking job opportunities involing software engineering,
+web design, machine learning, and or data science.`;
+
+{
+  /* styles */
+}
 const styles = {
-    root: {
-        flexGrow: 1,
-        height: '100vh',
-        display: 'flex'
-    },
-    grid: {
-        color: 'black',
-        backgroundColor: 'white'
-    },
-    gridleft: {
-        backgroundColor: 'white'
-
-    },
-    gridright: {
-        backgroundColor: 'white'
-    },
+  root: {
+    flexGrow: 1,
+    height: "100vh",
+    display: "flex"
+  },
+  grid: {},
+  gridleft: {
+    backgroundColor: "dimgray"
+  },
+  gridright: {
+    backgroundColor: "dimgray"
+  }
 };
 
 class About extends React.Component {
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className={classes.root}>
-                <Grid container spacing={0} className={classes.grid}>
-                    <Grid item xs={5} className={classes.gridleft}>
-                        <img src={require('../images/peterpropic.jpg')} alt="Peter" className="aboutimg" />
-                    </Grid>
-                    <Grid item xs={7} className={classes.gridright}>
-                        <div className="aboutdiv">
-                            <h1 className="aboutheader">Hello !</h1>
-                            <h2 className="aboutsubheader"> My name is Peter</h2>
-                        <p className="aboutpara">
-                                Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
-                                dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
-                                Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
-                                sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
-                        </p>
-                        </div>
-                    </Grid>
-                </Grid>
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={0} className={classes.grid}>
+          <Grid item xs={6} className={classes.gridleft}>
+            <div className="aboutdivleft">
+              <img
+                src={require("../images/peterpropic.jpg")}
+                alt="Peter"
+                className="aboutimg"
+              />
             </div>
-        )
-    }
-
+          </Grid>
+          <Grid item xs={6} className={classes.gridright}>
+            <div className="aboutdivright">
+              <h1 className="aboutheader"> About Me. </h1>
+              <p className="aboutpara">{aboutparaText}</p>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(About);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,87 +1,98 @@
-import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import Grid from '@material-ui/core/Grid';
-import {withStyles} from "@material-ui/core";
+import React from "react";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core";
 import IconLink from "./IconLink";
 import Avatar from "@material-ui/core/Avatar";
 
-
-{/* styles */}
+{
+  /* styles */
+}
 const styles = {
-    root:{
-        marginLeft: '18%', // margins, padding, borders
-        marginRight: '18%',
-        marginTop: '3%',
-        marginBottom: '3%',
+  root: {
+    marginLeft: "18%", // margins, padding, borders
+    marginRight: "18%",
+    marginTop: "3%",
+    marginBottom: "3%",
 
-        display: 'flex', // display
+    display: "flex", // display
 
-        textAlign: 'center',
-        textDecorationColor: 'white',
-        color: 'white'
-    },
-    grid:{
-        backgroundColor: 'rgba(0,0,0,.7)', //color
+    textAlign: "center",
+    textDecorationColor: "white",
+    color: "white"
+  },
+  grid: {
+    backgroundColor: "rgba(0,0,0,.7)", //color
 
-        padding: '4%', // margins, padding, borders
-        borderRadius: '25px',
-        margin: 'auto',
+    padding: "4%", // margins, padding, borders
+    borderRadius: "25px",
+    margin: "auto",
 
-        fontSize: '24px', // text/color
+    fontSize: "24px", // text/color
 
-        align: 'center', // alignment
+    align: "center" // alignment
+  },
+  bigAvatar: {
+    marginLeft: "auto", // margins, padding, borders
+    marginRight: "auto",
 
-
-    },
-    bigAvatar:{
-        marginLeft: 'auto', // margins, padding, borders
-        marginRight: 'auto',
-
-        width: '150px', // size
-        height: '150px',
-
-    },
-    links: {
-        fontSize: '50px', // text/color
-    },
-
+    width: "150px", // size
+    height: "150px"
+  },
+  links: {
+    fontSize: "50px" // text/color
+  }
 };
 
 // Home Page
 class Home extends React.Component {
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className={classes.root}>
-                <Grid  className={classes.grid}>
-                    <Grid item xs={12}>
-                        <Avatar src={require('../images/peterpropic.jpg')} alt="Peter" className={classes.bigAvatar}/>
-                        <h1>Peter Regan</h1>
-                        <h2>Aspiring Software Engineer</h2>
-                        <h1>. . . . . . . . . . . . . . . . . . . . . . . . .</h1>
-                        <h2>Java | Javascript | React | Python | R</h2>
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Grid className={classes.grid}>
+          <Grid item xs={12}>
+            <Avatar
+              src={require("../images/peterpropic.jpg")}
+              alt="Peter"
+              className={classes.bigAvatar}
+            />
+            <h1>Peter Regan</h1>
+            <h2>Aspiring Software Engineer</h2>
+            <h1>. . . . . . . . . . . . . . . . . . . . . . .</h1>
+            <h2>Java | Javascript | React | Python | R</h2>
 
             {/*social links*/}
             <div className={classes.links}>
-                {/*Linked in*/}
-                <IconLink link="https://www.linkedin.com/in/peter-regan-590886162/" icon={<LinkedInIcon className={classes.links}/>}/>
-                {/*Instagram*/}
-                <IconLink link="https://www.instagram.com/pregan14/" icon={<InstagramIcon className={classes.links}/>}/>
-                {/*Git Hub*/}
-                <IconLink link="https://github.com/peterregan" icon={<GitHubIcon className={classes.links}/>} />
-                {/*Facebook*/}
-                <IconLink link="https://www.facebook.com/peter.regan.3511" icon={<FacebookIcon className={classes.links}/>}/>
+              {/*Linked in*/}
+              <IconLink
+                link="https://www.linkedin.com/in/peter-regan-590886162/"
+                icon={<LinkedInIcon className={classes.links} />}
+              />
+              {/*Instagram*/}
+              <IconLink
+                link="https://www.instagram.com/pregan14/"
+                icon={<InstagramIcon className={classes.links} />}
+              />
+              {/*Git Hub*/}
+              <IconLink
+                link="https://github.com/peterregan"
+                icon={<GitHubIcon className={classes.links} />}
+              />
+              {/*Facebook*/}
+              <IconLink
+                link="https://www.facebook.com/peter.regan.3511"
+                icon={<FacebookIcon className={classes.links} />}
+              />
             </div>
-                </Grid>
-                </Grid>
-            </div>
-        )
-    }
-
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(Home);
